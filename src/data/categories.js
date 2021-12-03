f6a88546456fd53345705d8ad9b10248f83d712a
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-12-02 21:36:10 -0500

    list of categories in the game

---
+++ src/data/categories.js
@@ -0,0 +1,35 @@
+export default categories = [
+  {
+    'Fast Food': [
+      'Panera Bread',
+      'Wendys',
+      'Dominos Pizza',
+      'Dunkin Donuts',
+      'Taco Bell',
+      'Chipotle Mexican Grill',
+    ],
+    Animals: [
+      'Parakeet',
+      'Golden Retriever',
+      'Wolf',
+      'Guinea Pig',
+      'Polar Bear',
+      'Mountain Goat',
+    ],
+    Countries: [
+      'Hong Kong',
+      'Jamaica',
+      'Puerto Rico',
+      'French Polynesia',
+      'Australia',
+      'Malaysia',
+    ],
+    Movies: [
+      'The Addams Family',
+      'Harry Potter and the Chamber of Secrets',
+      'The Amazing Spider-Man',
+      'Space Jam',
+      'Godzilla vs. Kong',
+    ],
+  },
+];
