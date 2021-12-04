42fcbb87e4ea0ae4c0ce3b8dd74f483d1292bfff
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-12-03 21:32:47 -0500

    changed filename to data

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+const data = {
+  'Fast Food': [
+    'Panera Bread',
+    'Wendys',
+    'Dominos Pizza',
+    'Dunkin Donuts',
+    'Taco Bell',
+    'Chipotle Mexican Grill',
+  ],
+  Animals: [
+    'Parakeet',
+    'Golden Retriever',
+    'Wolf',
+    'Guinea Pig',
+    'Polar Bear',
+    'Mountain Goat',
+  ],
+  Countries: [
+    'Hong Kong',
+    'Jamaica',
+    'Puerto Rico',
+    'French Polynesia',
+    'Australia',
+    'Malaysia',
+  ],
+  Movies: [
+    'The Addams Family',
+    'Harry Potter and the Chamber of Secrets',
+    'The Amazing Spider-Man',
+    'Space Jam',
+    'Godzilla vs. Kong',
+  ],
+};
+
+export default data;
